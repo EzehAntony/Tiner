910fa691deb1cc64a791c169f48d0ba35d49cb46
Anthony <58951485+EzehAntony@users.noreply.github.com>
2022-09-20 18:50:52 +0100

    start redirect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 
 function Card() {
   return (
-    <div className='card'></div>
-  )
+    <div className="card">
+      <div className="up">
+        <img src="/henessy.jpg" alt="" />
+      </div>
+      <div className="down">
+        <h3 className="name">Tatiana Saris</h3>
+        <p className="location">Florida, USA </p>
+        <p className="age">21</p>
+
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
