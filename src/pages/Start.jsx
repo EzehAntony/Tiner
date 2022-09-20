a6357cafbbef3995dc18cd98c22ad81fd7072091
Anthony <58951485+EzehAntony@users.noreply.github.com>
2022-09-20 16:44:54 +0100

    start redirect

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useEffect } from "react";
 import "./Start.css";
-import {ClapSpinner} from "react-spinners-kit"
+import { useNavigate } from "react-router-dom";
+import { ClapSpinner } from "react-spinners-kit";
 
 function Start() {
+  const navigate = useNavigate();
   useEffect(() => {
     document
       .querySelector("meta[name='theme-color']")
       .setAttribute("content", "#F62355");
+
+    setTimeout(() => {
+      navigate("/home");
+    }, 2000);
   }, []);
   return (
     <div className="startPage">
